2bbfd77014288e538578edec4efd48eb6680038d
JunarisAlf <junaris0715@gmail.com>
2022-12-18 20:01:26 +0700

    Create sales table migration

--- app/Http/Controllers/Auth/SalesAuthController.php
+++ app/Http/Controllers/Auth/SalesAuthController.php
@@ -9,9 +9,12 @@ class SalesAuthController extends Controller
 {
     public function register(Request $request){
         $validatedRes = $request->validate([
+            'noWa' => 'required',
+            'fullName' => 'required',
             'password' => 'required|confirmed|min:6',
             'email' => 'required|email'
         ]);
+        
         dd($validatedRes);
     }
 }
